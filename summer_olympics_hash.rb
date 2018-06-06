faea9cd26aa12110df29a69e3a3f296206fea077
JewelsCP <giulianacarballoparker@gmail.com>
2018-06-06 22:31:36 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -3,7 +3,7 @@ def create_olympics_hash {"Sydney" => 2000, "Athens" => 2004, "Beijing" => 2008,
   # Implement this method so that it returns a hash with the data provided on README.md
 end
 
-def add_a_key_value_pair
+def add_a_key_value_pair 
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
 end
 
